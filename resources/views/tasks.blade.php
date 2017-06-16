58c15ab17c07cb927739ba8686df3e54b78253d6
diegocontrerasgarza <diego.97m@gmail.com>
2017-06-16 00:04:14 -0500

    Add files via upload

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML> 
+
+<html>
+
+  <head>
+
+    <title></title>
+
+  </head>
+
+  <body>
+
+    <ul>
+    <?php foreach ($tasks as $task) : ?>
+
+        <li><?= $task; ?></li>
+    <?php endforeach; ?>
+</ul>
+  </body>
+
+</html>
